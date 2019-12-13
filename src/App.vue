ae9b513ebc16c50ea5beb454277bd9bb872680f6
bianxiaokai <bianxiaokai@100tal.com>
2019-12-13 16:48:20 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,122 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <transition :name="transition">
+      <router-view class="Router"></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    data() {
+      return {
+        transition: 'fade'
+      }
+    },
+    watch: {
+      '$route'(to, from) {//根据路由确定是返回还是前进，默认动画为fade
+        if (from.meta && from.meta.direction === 'slide-left') {
+          this.transition = 'slide-right';
+          if (to.meta.direction === 'slide-left') {
+            this.transition = to.meta.direction
+          }
+          if (
+            (to.name === 'like' && from.name === 'likeCompany') ||
+            (to.name === 'articleMine' && from.name === 'editArticleMine')
+          ) {
+            this.transition = 'slide-right';
+          }
+        } else {
+          this.transition = to.meta.direction || 'fade';
+        }
+
+      }
+    },
+    mounted() {
+      if (!this.commonUtils.isPhone()) {
+        this.$messageBox('请使用手机访问哦')
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  html {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+
+  a {
+    text-decoration: none;
+    color: #222222;
+  }
+
+  a:link {
+    text-decoration: none;
+  }
+
+  a:visited {
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+
+  #app {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+
+  }
+
+  @font-face {
+    font-family: 'big-font';
+    src: url("./assets/fonts/top_font.ttf") format("truetype");
+  }
+
+  .Router {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    transition: all .377s ease;
+    box-sizing: border-box;
+    overflow: auto;
+  }
+
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
+  }
+
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+
+  .slide-left-enter,
+  .slide-right-leave-active {
+    opacity: 0;
+    -webkit-transform: translate(100%, 0);
+    transform: translate(100%, 0);
+  }
+
+  .slide-left-leave-active,
+  .slide-right-enter {
+    opacity: 0;
+    -webkit-transform: translate(-100%, 0);
+    transform: translate(-100% 0);
+  }
+
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,34 @@
 import Vue from 'vue'
-import App from './App.vue'
+import store from './store'
+import Mint, {DatetimePicker, MessageBox, Switch, Toast} from 'mint-ui';
+import App from './App'
+import router from './router'
+import config from './config'
+import BaseApi from './api/BaseApi'
+import commonUtils from './utils/commonUtils'
+import '../node_modules/swiper/css/swiper.min.css';
+import VueLazyload from 'vue-lazyload'
 
-Vue.config.productionTip = false
+Vue.component(DatetimePicker.name, DatetimePicker);
+Vue.component(Switch.name, Switch);
+if (config.MOCK_ENABLE) {
+  require('./data');//加载mock
+}
+require('./style/my-mint.scss');
+Vue.use(Mint);
+Vue.config.productionTip = false;
+Vue.prototype.BaseApi = BaseApi;//将BaseApi挂载在Vue中
+Vue.prototype.commonUtils = commonUtils;//将commonUtils挂载在Vue中
+Vue.prototype.$config = config;//将config挂载在Vue中
+Vue.prototype.$messageBox = MessageBox;//弹窗挂载
+Vue.prototype.$toast = Toast;//Toast挂载
+Vue.use(VueLazyload);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#root',
+  router,
+  store,
+  render:h=>h(App)
+});
+
+
